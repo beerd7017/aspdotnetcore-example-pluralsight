8fd31aeef23e5443c39874e626fa4f829b40aaf7
Dennis <destepp11@gmail.com>
2018-08-23 11:01:50 -0400

    Added JavaScript events.

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -4,5 +4,12 @@ var s = "";
 console.log("Hello Pluralsight");
 
 var theForm = document.getElementById("theForm");
-
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementById("product-props");
+var listItems = productInfo.item[0].children;
