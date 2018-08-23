21d72483598a8149e8320a21ac6808ada7e07d56
Dennis <destepp11@gmail.com>
2018-08-23 14:16:37 -0400

    Added some more jQuery.

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,17 +1,30 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
 
-console.log("Hello Pluralsight");
+    var s = "";
 
-var theForm = $("#theForm");
-theForm.hide();
+    console.log("Hello Pluralsight");
 
-var button = $("#buyButton");
-button.on("click", function () {
-    console.log("Buying Item");
-});
 
-var productInfo = $(".product-props li");
-productInfo.on("click", function() {
-    console.log("You clicked on " + $(this).text());
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click",
+        function () {
+            console.log("Buying Item");
+        });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click",
+        function () {
+            console.log("You clicked on " + $(this).text());
+        });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
 });
