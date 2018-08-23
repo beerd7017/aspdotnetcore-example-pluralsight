6ac65cd1784df526f8a6f035d6e5ac893f8ac3dc
Dennis <destepp11@gmail.com>
2018-08-23 13:32:52 -0400

    Added jQuery.

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -3,13 +3,15 @@ var s = "";
 
 console.log("Hello Pluralsight");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+var theForm = $("#theForm");
+theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
+var button = $("#buyButton");
+button.on("click", function () {
     console.log("Buying Item");
 });
 
-var productInfo = document.getElementById("product-props");
-var listItems = productInfo.item[0].children;
+var productInfo = $(".product-props li");
+productInfo.on("click", function() {
+    console.log("You clicked on " + $(this).text());
+});
