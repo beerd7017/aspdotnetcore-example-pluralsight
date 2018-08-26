cee503752c0c6fbf2e699fce80025e0c6469f967
Dennis <destepp11@gmail.com>
2018-08-26 12:56:43 -0400

    Designed the Login View

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using DutchTreat.Data;
+using DutchTreat.Data.Entities;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +26,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<StoreUser, IdentityRole>(cfg => { cfg.User.RequireUniqueEmail = true; }).AddEntityFrameworkStores<DutchContext>();
             services.AddDbContext<DutchContext>(cfg => { cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString")); });
             services.AddTransient<DutchSeeder>();
             services.AddScoped<IDutchRepository, DutchRepository>();
@@ -44,6 +47,7 @@ namespace DutchTreat
             }
 
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseNodeModules(env);
 
             app.UseMvc(cfg =>
